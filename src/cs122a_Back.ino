6c84174e09d737a316ff3aed8f266340d868dc17
Peter <Peter@Peters-MacBook-Air.local>
2015-12-14 22:22:24 -0800

    added files

---
+++ src/cs122a_Back.ino
@@ -0,0 +1,352 @@
+// testshapes demo for Adafruit RGBmatrixPanel library.
+// Demonstrates the drawing abilities of the RGBmatrixPanel library.
+// For 16x32 RGB LED matrix:
+// http://www.adafruit.com/products/420
+
+// Written by Limor Fried/Ladyada & Phil Burgess/PaintYourDragon
+// for Adafruit Industries.
+// BSD license, all text above must be included in any redistribution.
+
+#include "pitches.h"
+#include <Adafruit_GFX.h>   // Core graphics library
+#include <RGBmatrixPanel.h> // Hardware-specific library
+#include "TaskScheduler.h" //provided by at https://github.com/arkhipenko/TaskScheduler
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+
+#define CLK 8  // MUST be on PORTB!
+#define LAT A3
+#define OE  9
+#define A   A0
+#define B   A1
+#define C   A2
+RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
+
+int sensorValue = 101;
+bool EMERGENCY = false;
+int count = 0;
+//int pos = 0;
+int disp1 = 0; 
+int msg[1];
+RF24 radio(0,10);
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+
+//create callback methods
+void Distance_Callback();
+void DisplayMatrix_Callback();
+
+//Tasks
+Task DisplayMatrix(500, TASK_FOREVER, &DisplayMatrix_Callback);
+Task Distance(100, TASK_FOREVER, &Distance_Callback);
+
+Scheduler runner;
+
+#define inits 0
+#define slow 1
+#define right 2
+#define left 3
+#define normal 4
+#define none 5
+static int state = inits;
+
+void DisplayMatrix_Callback(){  
+  //read from SPI
+  disp1 = 0;
+
+  if (radio.available()){
+   bool done = false;    
+   while (!done){
+     done = radio.read(msg, 1);      
+     disp1 = msg[0];
+     Serial.println(msg[0]);
+     delay(10);}
+ }
+ else{Serial.println("No radio available");}
+
+  switch(state){  //ACTIONS
+    case inits:
+      break;
+    case slow:
+      //          SLOW SIGNAL     //
+      matrix.setCursor(2, 0);   // start at top left, with one pixel of spacing
+      matrix.setTextSize(1);    // size 1 == 8 pixels high
+    
+      // print each letter with a rainbow color
+      matrix.setTextColor(matrix.Color333(4,0,0));
+      matrix.print('S');
+      matrix.setTextColor(matrix.Color333(4,0,0));
+      matrix.print('L');
+      matrix.setTextColor(matrix.Color333(4,0,0));
+      matrix.print('O');
+      matrix.setTextColor(matrix.Color333(4,0,0));
+      matrix.print('W');
+      matrix.setTextColor(matrix.Color333(4,0,0));
+      matrix.print('!');
+
+      matrix.setCursor(2, 8);   // start at top left, with one pixel of spacing
+      matrix.setTextSize(1);    // size 1 == 8 pixels high
+    
+      // print each letter with a rainbow color
+      matrix.setTextColor(matrix.Color333(4,0,0));
+      matrix.print('D');
+      matrix.setTextColor(matrix.Color333(4,0,0));
+      matrix.print('O');
+      matrix.setTextColor(matrix.Color333(4,0,0));
+      matrix.print('W');
+      matrix.setTextColor(matrix.Color333(4,0,0));
+      matrix.print('N');
+      matrix.setTextColor(matrix.Color333(4,0,0));
+      matrix.print('!');
+      /*
+      if(pos > 30) pos = 0;
+      else pos++;
+
+      delay(100);
+      matrix.fillScreen(matrix.Color333(0, 0, 0));
+      */
+      if(EMERGENCY) tone(A4, NOTE_A6, 250);
+      break;
+    case right:
+      //        Right Turn      //
+      matrix.drawLine(15, 0, 31, 8, matrix.Color333(7, 0, 0));
+      matrix.drawLine(16, 0, 31, 7, matrix.Color333(7, 0, 0));
+      matrix.drawLine(17, 0, 31, 6, matrix.Color333(7, 0, 0));
+      matrix.drawLine(15, 14, 31, 6, matrix.Color333(7, 0, 0));
+      matrix.drawLine(16, 14, 31, 7, matrix.Color333(7, 0, 0));
+      matrix.drawLine(17, 14, 31, 8, matrix.Color333(7, 0, 0));
+    
+      matrix.fillRect(0, 6, 32, 3, matrix.Color333(7, 0, 0));
+      break;
+    case left:
+      //        Left Turn      //
+      matrix.drawLine(15, 0, 0, 8, matrix.Color333(7, 0, 0));
+      matrix.drawLine(16, 0, 0, 7, matrix.Color333(7, 0, 0));
+      matrix.drawLine(17, 0, 0, 6, matrix.Color333(7, 0, 0));
+      matrix.drawLine(15, 14, 0, 6, matrix.Color333(7, 0, 0));
+      matrix.drawLine(16, 14, 0, 7, matrix.Color333(7, 0, 0));
+      matrix.drawLine(17, 14, 0, 8, matrix.Color333(7, 0, 0));
+    
+      matrix.fillRect(0, 6, 32, 3, matrix.Color333(7, 0, 0));
+      break;
+    case normal:
+      matrix.fillRect(7, 4, 18, 8, matrix.Color333(7, 0, 0));
+      break;
+    case none:
+      matrix.fillScreen(matrix.Color333(0, 0, 0));
+      noTone(A4);
+      break;
+  }
+
+  switch(state){  //STATE CHANGING
+    case inits:
+      state = none;
+      break;
+    case slow:
+      //          SLOW SIGNAL     //
+      state = none;
+      count = 0;
+      break;
+    case right:
+      //          RIGHT SIGNAL     //
+      state = none;
+      break;
+    case left:
+      //          LEFT SIGNAL     //
+      state = none;
+      break;
+    case normal:
+      //          DEFAULT SIGNAL     //
+      state = none;
+      break;
+    case none:
+      //          NO SIGNAL     //
+      if((disp1 == 3) || EMERGENCY) state = slow;
+      else if(disp1 == 2) state = right;
+      else if(disp1 == 1) state = left;
+      else if(disp1 == 5) state = normal;
+      break;
+  }
+}
+
+void Distance_Callback(){  
+  sensorValue = analogRead(A5);
+  //Serial.println(sensorValue);
+
+  if(sensorValue < 100){
+    if(count < 2) count++;
+    else {
+      EMERGENCY = true;
+      count = 0;
+    }
+  }
+  else {
+    if(count < 3) count++;
+    else {
+      EMERGENCY = false;
+      count = 0;
+    }
+  }
+}
+
+void setup() {
+  //set rate to 9600bps
+  Serial.begin(9600); //set rate to read from distance sensor
+  radio.begin();
+  radio.openReadingPipe(1,pipe);
+  radio.startListening();
+
+  Serial.println("Scheduler TEST");
+  runner.init();
+  Serial.println("Initialized scheduler");
+  
+  runner.addTask(Distance);
+  Serial.println("added t1");
+  runner.addTask(DisplayMatrix);
+  Serial.println("added t2");
+
+  delay(2000);
+  
+  Distance.enable();
+  Serial.println("Enabled t1");
+  DisplayMatrix.enable();
+  Serial.println("Enabled t2");
+
+  matrix.begin();
+  // fill the screen with 'black'
+  matrix.fillScreen(matrix.Color333(0, 0, 0));
+}
+
+void loop() { 
+  runner.execute();
+  /*
+ if (radio.available()){
+   bool done = false;    
+   while (!done){
+     done = radio.read(msg, 1);      
+     Serial.println(msg[0]);
+     delay(10);}
+ }
+ else{Serial.println("No radio available");}
+ */
+}
+
+
+
+/*
+  if((disp1 == HIGH && disp2 == HIGH) || (EMERGENCY)){
+    //          SLOW SIGNAL     //
+    matrix.setCursor(5 - pos, 1);   // start at top left, with one pixel of spacing
+    matrix.setTextSize(2);    // size 1 == 8 pixels high
+  
+    // print each letter with a rainbow color
+    matrix.setTextColor(matrix.Color333(4,0,0));
+    matrix.print('S');
+    matrix.setTextColor(matrix.Color333(4,0,0));
+    matrix.print('L');
+    matrix.setTextColor(matrix.Color333(4,0,0));
+    matrix.print('O');
+    matrix.setTextColor(matrix.Color333(4,0,0));
+    matrix.print('W');
+    if(pos > 30) pos = 0;
+    else pos++;
+  
+    delay(100);
+    matrix.fillScreen(matrix.Color333(0, 0, 0));
+  }
+
+  else if(disp1 == HIGH && disp2 == LOW){
+    //        Right Turn      //
+    matrix.drawLine(15, 0, 31, 8, matrix.Color333(7, 0, 0));
+    matrix.drawLine(16, 0, 31, 7, matrix.Color333(7, 0, 0));
+    matrix.drawLine(17, 0, 31, 6, matrix.Color333(7, 0, 0));
+    matrix.drawLine(15, 14, 31, 6, matrix.Color333(7, 0, 0));
+    matrix.drawLine(16, 14, 31, 7, matrix.Color333(7, 0, 0));
+    matrix.drawLine(17, 14, 31, 8, matrix.Color333(7, 0, 0));
+  
+    matrix.fillRect(0, 6, 32, 3, matrix.Color333(7, 0, 0));
+    
+    delay(500);
+    matrix.fillScreen(matrix.Color333(0, 0, 0));
+    delay(500);
+  }
+
+  else if(disp1 == LOW && disp2 == HIGH){
+    //        Left Turn      //
+    matrix.drawLine(15, 0, 0, 8, matrix.Color333(7, 0, 0));
+    matrix.drawLine(16, 0, 0, 7, matrix.Color333(7, 0, 0));
+    matrix.drawLine(17, 0, 0, 6, matrix.Color333(7, 0, 0));
+    matrix.drawLine(15, 14, 0, 6, matrix.Color333(7, 0, 0));
+    matrix.drawLine(16, 14, 0, 7, matrix.Color333(7, 0, 0));
+    matrix.drawLine(17, 14, 0, 8, matrix.Color333(7, 0, 0));
+  
+    matrix.fillRect(0, 6, 32, 3, matrix.Color333(7, 0, 0));
+    
+    delay(500);
+    matrix.fillScreen(matrix.Color333(0, 0, 0));
+    delay(500);
+  }
+
+  else  matrix.fillScreen(matrix.Color333(0, 0, 0));  //draw square in middle
+  */
+
+
+  
+/*
+  switch(state){  //STATE CHANGING
+    case inits:
+      state = none;
+      count = 0;
+      break;
+    case slow:
+      //          SLOW SIGNAL     //
+      if((disp1 == 333) ) state = slow;
+      else state = none;
+      count = 0;
+      break;
+    case right:
+      //          RIGHT SIGNAL     //
+      count++;
+      if(count >= 5) {
+        state = right;
+        count = 0;
+      }
+      else state = none;
+      break;
+    case left:
+      //          LEFT SIGNAL     //
+      count++;
+      if(count >= 5) {
+        state = left;
+        count = 0;
+      }
+      else state = none;
+      break;
+    case normal:
+      //          DEFAULT SIGNAL     //
+      count++;
+      if(count >= 5) {
+        state = normal;
+        count = 0;
+      }
+      else state = none;
+      break;
+    case none:
+      //          NO SIGNAL     //
+      count++;
+      if((disp1 == 3)) state = slow;
+      else if(disp1 == 2 && count >= 5) {
+        state = right;
+        count = 0;
+      }
+      else if(disp1 == 1 && count >= 5) {
+        state = left;
+        count = 0;
+      }
+      else if(count >= 5) {
+        state = normal;
+        count = 0;
+      }
+      else state = none;
+      break;
+  } */
---
+++ src/cs122a_Front.ino
@@ -0,0 +1,177 @@
+#include <LiquidCrystal.h>
+#include "pitches.h"
+#include "TaskScheduler.h" //provided by at https://github.com/arkhipenko/TaskScheduler
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+
+//initialize lcd pins
+//LiquidCrystal lcd(12, 11, 2, 3, 4, 5);
+LiquidCrystal lcd(A3, 3, 4, 5, 6, 7);
+
+// value read from the photoresistor
+int sensorValue = 0;
+
+//transiever intialization
+int msg[1];
+RF24 radio(9, 10);
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+//int SW1 = 3;
+
+//signal variables
+int leftSignal, rightSignal;
+
+//hall effect initializations
+volatile byte half_revolutions;
+unsigned int rpm;
+unsigned long timeold;
+
+//create callback methods
+void DisplayLCD_Callback();
+void FlashLight_Callback();
+//void SensorRead_Callback();
+void DisplayMatrix_Callback();
+void Hall_Callback();
+
+//Tasks
+Task DisplayLCD(100, TASK_FOREVER, &DisplayLCD_Callback);
+Task FlashLight(3000, TASK_FOREVER, &FlashLight_Callback);
+//Task SensorRead();
+Task DisplayMatrix(100, TASK_FOREVER, &DisplayMatrix_Callback);
+Task Hall(1, TASK_FOREVER, &Hall_Callback);
+
+Scheduler runner;
+
+void DisplayLCD_Callback() {
+  lcd.setCursor(5, 0);
+  // print the mph
+  double mph = rpm*188.4;
+  mph = mph/5280;
+  lcd.print(mph);
+
+  // print the if rightSignal is on
+  lcd.setCursor(2,1);
+  if(rightSignal == HIGH) lcd.print('R');
+  else lcd.print(' ');
+
+  // print the if leftSignal is on
+  lcd.setCursor(0,1);
+  if(leftSignal == HIGH) lcd.print('L');
+  else lcd.print(' ');
+    
+}
+void FlashLight_Callback() {
+  // read the analog in value:
+  sensorValue = analogRead(A0);
+  // map it to the range of the analog out:
+
+  // print the results to the serial monitor:
+  Serial.print("sensor = " );
+  Serial.print(sensorValue);
+  Serial.print("\n");
+
+  if (sensorValue < 200) {
+    digitalWrite(8, HIGH);
+  }
+  else digitalWrite(8, LOW);
+
+  // wait 2 milliseconds before the next loop
+  // for the analog-to-digital converter to settle
+  // after the last reading:
+  //delay(50);
+
+}
+void DisplayMatrix_Callback() {
+  rightSignal = digitalRead(A4);
+  leftSignal = digitalRead(A5);
+
+  if(rightSignal == HIGH && leftSignal == HIGH){
+    //Serial.println("both");
+    msg[0] = 3;
+    radio.write(msg, 1);
+  }
+  else if(rightSignal == HIGH && leftSignal == LOW){
+    //Serial.println("right");
+    msg[0] = 2;
+    radio.write(msg, 1);
+  }
+  else if(rightSignal == LOW && leftSignal == HIGH){
+    //Serial.println("left");
+    msg[0] = 1;
+    radio.write(msg, 1);
+  }
+  else{
+    //Serial.println("none");
+    msg[0] = 5;
+    radio.write(msg, 1);
+  }
+}
+
+void Hall_Callback() {
+  if (half_revolutions >= 20) { 
+     rpm = 30*1000/(millis() - timeold)*half_revolutions;
+     timeold = millis();
+     half_revolutions = 0;
+     Serial.print("RPM: ");
+     Serial.println(rpm,DEC);
+   }
+}
+
+void magnet_detect()//This function is called whenever a magnet/interrupt is detected by the arduino
+ {
+   half_revolutions++;
+   Serial.println("detect");
+ }
+
+void setup() {
+  //set rate to 9600bps
+  Serial.begin(9600);
+
+  //start transiever code
+  radio.begin();
+  radio.openWritingPipe(pipe);
+
+  pinMode(8, OUTPUT); //set pin for flashlight
+  pinMode(A4, INPUT); //set pin for right signal
+  pinMode(A5, INPUT); //set pin for left signal
+
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("MPH: ");
+
+  attachInterrupt(0, magnet_detect, RISING);//Initialize the intterrupt pin (Arduino digital pin 2)
+  half_revolutions = 0;
+  rpm = 0;
+  timeold = 0;
+
+  //start scheduler code
+  Serial.println("Scheduler TEST");
+  runner.init();
+  Serial.println("Initialized scheduler");
+
+  runner.addTask(DisplayLCD);
+  Serial.println("added t1");
+  runner.addTask(FlashLight);
+  Serial.println("added t2");
+  runner.addTask(DisplayMatrix);
+  Serial.println("added t3");
+  runner.addTask(Hall);
+  Serial.println("added t4");
+
+  delay(2000);
+
+  DisplayLCD.enable();
+  Serial.println("Enabled t1");
+  FlashLight.enable();
+  Serial.println("Enabled t2");
+  DisplayMatrix.enable();
+  Serial.println("Enabled t3");
+  Hall.enable();
+  Serial.println("Enabled t4");
+}
+
+void loop() {
+  runner.execute();
+}
+
